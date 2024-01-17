55471a44e7edbf0cccbb6a7f8b59dae5915109e3
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2024-01-16 21:49:00 -0300

    Update time.php

--- teste/time.php
+++ teste/time.php
@@ -1,7 +1,8 @@
 <?php
 
 date_default_timezone_set('America/Sao_Paulo');
-echo time();
-$dataAtual = date("Y-m-d H:i:s", time());
-echo $dataAtual;
+
+$dataAtual = date("Y-m-d H:i:s", 1705452444);
+$dataAtual2 = date("Y-m-d H:i:s", 1705456044);
+echo $dataAtual.$dataAtual2;
 ?>
