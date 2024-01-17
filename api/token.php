2fa4fb9f18a1d6b4f23f5e755ac9fbe5d0837034
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2024-01-16 21:36:44 -0300

    Update token.php

--- api/token.php
+++ api/token.php
@@ -5,8 +5,7 @@ use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 $jwt = $_GET['token'];
 $key = 'kljsjdlkajl#KJKL#k3j4lkj4kl2jkl34kJL$#wq423lk4jlk23JKL#@LK$';
-$headers = new stdClass();
-$decoded = JWT::decode($jwt, new Key($key, 'HS256'), $headers);
+$decoded = JWT::decode($jwt, new Key($key, 'HS256'));
 
 echo json_encode($decoded);
 
