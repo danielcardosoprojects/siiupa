d660ead3fbd8d9ba401548fd8a617cecb63d0921
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2024-01-16 21:58:13 -0300

    Update token.php

--- api/token.php
+++ api/token.php
@@ -3,6 +3,9 @@ header("Access-Control-Allow-Origin: *");
 require '../vendor/autoload.php';
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
+if($_POST['token']){
+    http_response_code(403);
+}
 $jwt = $_POST['token'];
 $key = 'kljsjdlkajl#KJKL#k3j4lkj4kl2jkl34kJL$#wq423lk4jlk23JKL#@LK$';
 $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
