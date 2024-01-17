758663f022e0eaec5a1e4bebb9cf2d3765b05083
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2024-01-16 21:06:08 -0300

    Update jwt.php

--- teste/jwt.php
+++ teste/jwt.php
@@ -24,7 +24,7 @@ print_r($jwt);
 // Pass a stdClass in as the third parameter to get the decoded header values
 $headers = new stdClass();
 $decoded = JWT::decode($jwt, new Key($key, 'HS256'), $headers);
-print_r($headers);
+print_r($decoded);
 
 /*
  NOTE: This will now be an object instead of an associative array. To get
