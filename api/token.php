4f108f3e2147a809f6b501ea12ffbb81788710f7
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2024-01-16 22:00:00 -0300

    Update token.php

--- api/token.php
+++ api/token.php
@@ -5,7 +5,8 @@ use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 if(!$_POST['token']){
     http_response_code(403);
-    die;
+    exit;
+
 }
 $jwt = $_POST['token'];
 $key = 'kljsjdlkajl#KJKL#k3j4lkj4kl2jkl34kJL$#wq423lk4jlk23JKL#@LK$';
