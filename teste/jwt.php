a9f61f9f135c377d7b05236a4b5fe76b4ae7745e
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2024-01-16 21:04:52 -0300

    Update jwt.php

--- teste/jwt.php
+++ teste/jwt.php
@@ -1,29 +1,45 @@
 <?php
 require '../vendor/autoload.php';
 use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 $key = 'example_key';
 $payload = [
-    'iss' => 'https://www.siupa.com.br',
-    'aud' => 'https://www.siupa.com.br',
-    'usuario' => 'Daniel Cardoso',
+    'iss' => 'http://example.org',
+    'aud' => 'http://example.com',
     'iat' => 1356999524,
     'nbf' => 1357000000
 ];
 
-$headers = [
-    'x-forwarded-for' => 'www.google.com'
-];
-
-// Encode headers in the JWT string
-$jwt = JWT::encode($payload, $key, 'HS256', null, $headers);
-
-// Decode headers from the JWT string WITHOUT validation
-// **IMPORTANT**: This operation is vulnerable to attacks, as the JWT has not yet been verified.
-// These headers could be any value sent by an attacker.
-list($headersB64, $payloadB64, $sig) = explode('.', $jwt);
-$decoded = json_decode(base64_decode($headersB64), true);
-
+/**
+ * IMPORTANT:
+ * You must specify supported algorithms for your application. See
+ * https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40
+ * for a list of spec-compliant algorithms.
+ */
+$jwt = JWT::encode($payload, $key, 'HS256');
+$decoded = JWT::decode($jwt, new Key($key, 'HS256'));
 print_r($jwt);
-var_dump($decoded);
+
+// Pass a stdClass in as the third parameter to get the decoded header values
+$headers = new stdClass();
+$decoded = JWT::decode($jwt, new Key($key, 'HS256'), $headers);
+print_r($headers);
+
+/*
+ NOTE: This will now be an object instead of an associative array. To get
+ an associative array, you will need to cast it as such:
+*/
+
+$decoded_array = (array) $decoded;
+
+/**
+ * You can add a leeway to account for when there is a clock skew times between
+ * the signing and verifying servers. It is recommended that this leeway should
+ * not be bigger than a few minutes.
+ *
+ * Source: http://self-issued.info/docs/draft-ietf-oauth-json-web-token.html#nbfDef
+ */
+JWT::$leeway = 60; // $leeway in seconds
+$decoded = JWT::decode($jwt, new Key($key, 'HS256'));
 ?>
