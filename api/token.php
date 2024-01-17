2e2d6c5b631eca7e7db46ad61c9773a298c59c97
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2024-01-16 22:10:03 -0300

    Update token.php

--- api/token.php
+++ api/token.php
@@ -13,13 +13,14 @@ $jwt = $_POST['token'];
 $key = 'kljsjdlkajl#KJKL#k3j4lkj4kl2jkl34kJL$#wq423lk4jlk23JKL#@LK$';
 try {
 $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
-echo json_encode($decoded);
+
 } catch (LogicException $e) {
     // errors having to do with environmental setup or malformed JWT Keys
 } catch (UnexpectedValueException $e) {
     // errors having to do with JWT signature and claims
     echo "Erro: " . $e->getMessage();
+    exit;
 }
-
+echo json_encode($decoded);
 
 ?>
