79e874218287f08ed67e42e8f224e59f6530cf60
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2024-01-18 00:15:13 -0300

    Update arquivo.js

--- teste/cep/arquivo.js
+++ teste/cep/arquivo.js
@@ -158,7 +158,7 @@ function atualizarPosicao() {
     var novaPosicaoY = (campoPosicao.value - 24);
     if (novaPosicaoY !== posicaoY) {
         posicaoY = novaPosicaoY;
-        container.style.backgroundPosition = "-597px " + posicaoY + "px";
+        container.style.backgroundPosition = "-500px " + posicaoY + "px";
     }
 }
 
