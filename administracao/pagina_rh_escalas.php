d04092b04fd9b800c269ea48c84978dfe02004b7
danielcadol <121764444+danielcardosoprojects@users.noreply.github.com>
2024-01-18 11:39:04 -0300

    Update pagina_rh_escalas.php

--- administracao/pagina_rh_escalas.php
+++ administracao/pagina_rh_escalas.php
@@ -39,7 +39,7 @@ include_once('tabelas.php');
         $('#btcriarescala').click(function() {
 
             $.confirm({
-                title: 'Criar escalassss',
+                title: 'Criar escalas',
                 content: 'url:administracao/escalas/listasetor.php?acao=criar',
                 columnClass: 'medium',
                 buttons: {
