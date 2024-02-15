dc61fa1119e4a56c73f1774d4bf422eeaaecf83b
danielcadol <121764444+danielcardosoprojects@users.noreply.github.com>
2024-02-15 17:35:57 -0300

    Update atualiza_matricula.php

--- administracao/api/atualiza_matricula.php
+++ administracao/api/atualiza_matricula.php
@@ -3,53 +3,72 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Content-Type');
 
 ?>
-<script>fetch("http://siupa.com.br/siiupa/api/rh/api.php/records/tb_funcionario?filter=status,eq,ativo&order=id,desc", {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-   /*body: JSON.stringify({
-        username: 'danielcardoso',
-        password: 'c*123c12'
-    }),*/
-})
-.then(response => response.json())
-.then(data => {
-    
-    data.records.forEach((item) => {
+<script>
+    fetch("http://siupa.com.br/siiupa/api/rh/api.php/records/tb_funcionario?filter=status,eq,ativo&order=id,desc", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            /*body: JSON.stringify({
+                 username: 'danielcardoso',
+                 password: 'c*123c12'
+             }),*/
+        })
+        .then(response => response.json())
+        .then(data => {
 
-        let ncpf = item.cpf.replace(/[^\d.-]/g, '');
+            data.records.forEach((item) => {
 
-    // Remove múltiplos pontos e traços consecutivos
-    ncpf = ncpf.replace(/([.-])\1+/g, '$1');
+                let ncpf = item.cpf.replace(/[^\d.-]/g, '');
 
-        consultaMatricula(ncpf);
-    });
-})
-.catch(error => {
-    // Lide com erros aqui
-    console.error('Erro na solicitação:', error);
-});
+                // Remove múltiplos pontos e traços consecutivos
+                ncpf = ncpf.replace(/([.-])\1+/g, '$1');
+
+                consultaMatricula(ncpf);
+            });
+        })
+        .catch(error => {
+            // Lide com erros aqui
+            console.error('Erro na solicitação:', error);
+        });
 
 
-function consultaMatricula(cpf){
-    fetch(`http://siupa.com.br/siiupa/administracao/api/consulta_matricula.php?cpf=${cpf}`, {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-   /*body: JSON.stringify({
-        username: 'danielcardoso',
-        password: 'c*123c12'
-    }),*/
-})
-//.then(response => response.json())
-.then(data => {
-    console.log(data);
-})
-.catch(error => {
-    // Lide com erros aqui
-    console.error('Erro na solicitação:', error);
-});
-}
+    function consultaMatricula(cpf) {
+        let ncpf = manterApenasNumeros(cpf);
+        fetch(`http://siupa.com.br/siiupa/administracao/api/consulta_matricula.php?cpf=${ncpf}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                /*body: JSON.stringify({
+                     username: 'danielcardoso',
+                     password: 'c*123c12'
+                 }),*/
+            })
+            //.then(response => response.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                // Lide com erros aqui
+                console.error('Erro na solicitação:', error);
+            });
+    }
+
+    function manterApenasNumeros(str) {
+        var resultado = '';
+
+        for (var i = 0; i < str.length; i++) {
+            var caractereAtual = str.charAt(i);
+
+            if (!isNaN(caractereAtual)) {
+                resultado += caractereAtual;
+            }
+        }
+
+        return resultado;
+    }
+
+    // Exemplo de uso
+ 
 </script>
