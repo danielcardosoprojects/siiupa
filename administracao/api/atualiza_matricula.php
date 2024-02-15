14c0f20966931aaaabd995f0085730dc03cfd34c
danielcadol <121764444+danielcardosoprojects@users.noreply.github.com>
2024-02-15 17:17:24 -0300

    Update atualiza_matricula.php

--- administracao/api/atualiza_matricula.php
+++ administracao/api/atualiza_matricula.php
@@ -17,10 +17,33 @@ header('Access-Control-Allow-Headers: Content-Type');
     
     data.records.forEach((item) => {
         console.log(item.cpf);
+        consultaMatricula(item.cpf);
     });
 })
 .catch(error => {
     // Lide com erros aqui
     console.error('Erro na solicitação:', error);
 });
+
+
+function consultaMatricula(cpf){
+    fetch(`http://siupa.com.br/siiupa/administracao/api/consulta_matricula.php?cpf=${cpf}`, {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+   /*body: JSON.stringify({
+        username: 'danielcardoso',
+        password: 'c*123c12'
+    }),*/
+})
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+})
+.catch(error => {
+    // Lide com erros aqui
+    console.error('Erro na solicitação:', error);
+});
+}
 </script>
