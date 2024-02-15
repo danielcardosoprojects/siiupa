c9bbbcf657995e6b6c375cd7e41c0173c9d0ae00
danielcadol <121764444+danielcardosoprojects@users.noreply.github.com>
2024-02-15 16:51:24 -0300

    Update atualiza_matricula.php

--- administracao/api/atualiza_matricula.php
+++ administracao/api/atualiza_matricula.php
@@ -14,7 +14,7 @@ header('Access-Control-Allow-Headers: Content-Type');
 })
 .then(response => response.json())
 .then(data => {
-    console.log(data.records[0]);
+    console.log(data.records[0].nome);
 })
 .catch(error => {
     // Lide com erros aqui
