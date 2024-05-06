7b7cc7ea329d696a1387b87789ab0e78ef034461
danielcadol <121764444+danielcardosoprojects@users.noreply.github.com>
2024-05-06 10:52:02 -0300

    Update pagina_escala_exibe.php

--- administracao/pagina_escala_exibe.php
+++ administracao/pagina_escala_exibe.php
@@ -129,6 +129,20 @@
         width: 100%;
         height: 100%;
     }
+
+
+    /* Estilo para a div que contém o conteúdo carregado pelo include */
+    #carregaesqueleto {
+        overflow-x: auto;
+        white-space: nowrap; /* Evita que o conteúdo quebre em várias linhas */
+    }
+
+    /* Estilo para o conteúdo dentro da div */
+    #carregaesqueleto > * {
+        display: inline-block; /* Garante que o conteúdo não quebre em várias linhas */
+        white-space: normal; /* Restaura o comportamento padrão de quebra de linha */
+    }
+
 </style>
 <?php
 
@@ -216,19 +230,7 @@ STATUS:
     <span id="excluir_varios">Excluir vários</span><span id="confirmar_exclusao" title="Confirmar exclusão" style='display:none'>✅</span><span id="cancelar_exclusao" style='display:none' title="Cancelar exclusão">❌</span>
 </p>
 
-<style>
-    /* Estilo para a div que contém o conteúdo carregado pelo include */
-    #carregaesqueleto {
-        overflow-x: auto;
-        white-space: nowrap; /* Evita que o conteúdo quebre em várias linhas */
-    }
 
-    /* Estilo para o conteúdo dentro da div */
-    #carregaesqueleto > * {
-        display: inline-block; /* Garante que o conteúdo não quebre em várias linhas */
-        white-space: normal; /* Restaura o comportamento padrão de quebra de linha */
-    }
-</style>
 <div id="carregaesqueleto">
 
     <?php
