ea0d9c6a086c0f3eebd1841ac4131d17303142d1
danielcadol <121764444+danielcardosoprojects@users.noreply.github.com>
2024-07-04 11:27:07 -0300

    Update pagina_rh_afastamento.php

--- administracao/pagina_rh_afastamento.php
+++ administracao/pagina_rh_afastamento.php
@@ -1,6 +1,6 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'].'/siiupa/bd/conectabd.php');
-include_once($_SERVER['DOCUMENT_ROOT'].'/siiupa/bd/nivel.php');
+//include_once($_SERVER['DOCUMENT_ROOT'].'/siiupa/bd/nivel.php');
 ?>
  <style>
         #loader {
