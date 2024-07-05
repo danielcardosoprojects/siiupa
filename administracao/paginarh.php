6fd07a4c6b06e4cb59a2ebe4a0f458bd37c5ee67
danielcadol <121764444+danielcardosoprojects@users.noreply.github.com>
2024-07-05 14:34:50 -0300

    Update paginarh.php

--- administracao/paginarh.php
+++ administracao/paginarh.php
@@ -378,9 +378,9 @@
             case 'rhfolhaadicionaservidor':
                 include_once('pagina_rh_folha_adicionaservidor.php');
                 break;
-                case 'rhcadastraferias':
-                    include_once('pagina_rh_cadastraferias.php');
-                    break;
+            case 'rhcadastraferias':
+                include_once('pagina_rh_cadastraferias.php');
+                break;
             default:
                 echo "<script>$(document).ready(function() {loadPage('pagina_rh_home');});</script>";
                 break;
