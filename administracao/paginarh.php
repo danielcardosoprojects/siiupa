664afc13419e245823a8f4974e2cfbacd27580fc
danielcadol <121764444+danielcardosoprojects@users.noreply.github.com>
2024-07-10 12:53:03 -0300

    Update paginarh.php

--- administracao/paginarh.php
+++ administracao/paginarh.php
@@ -325,7 +325,7 @@
 
         switch ($subsub) {
             case null:
-                echo "<script>$(document).ready(function() {loadPage('pagina_rh_home');});</script>";
+                echo "<script>alert('null');$(document).ready(function() {loadPage('pagina_rh_home');});</script>";
                 break;
             case 'perfil':
                 include_once('pagina_rh_perfil.php');
@@ -382,7 +382,7 @@
                 include('paginarh_cadastraferias.php');
                 break;
             default:
-                echo "<script>$(document).ready(function() {loadPage('pagina_rh_home');});</script>";
+                echo "<script>alert('default');$(document).ready(function() {loadPage('pagina_rh_home');});</script>";
                 break;
         }
 
