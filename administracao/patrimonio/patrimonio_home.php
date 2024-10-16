e2ccb0b48e643ba2ec21997a449b9aa71a337e80
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2024-10-16 00:57:22 -0300

    tt

--- administracao/patrimonio/patrimonio_home.php
+++ administracao/patrimonio/patrimonio_home.php
@@ -8,6 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Controle de Patrimônio</title>
+    <link href="style.css" rel="stylesheet">
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Axios JS -->
