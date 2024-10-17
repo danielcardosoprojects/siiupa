080ac03234f5e84db91fd21b343bba09d060f4a0
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2024-10-16 23:38:22 -0300

    Update imagem.php

--- administracao/patrimonio/imagem.php
+++ administracao/patrimonio/imagem.php
@@ -1,9 +1,6 @@
 <?php
 require '../../vendor/autoload.php';
 use Intervention\Image\ImageManagerStatic as Image;
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 // Obter o nome da imagem do parâmetro GET
 $imagem = $_GET['imagem'];
@@ -21,8 +18,8 @@ try {
     });
 
     // Definir o tipo de conteúdo e enviar a imagem
-    header('Content-Type: image/jpeg'); // Ajustar o tipo conforme necessário
-    return $img->response('jpeg');
+    header('Content-Type: image/jpeg');
+    echo $img->encode('jpeg');
 } catch (Exception $e) {
     // Tratar erros (imagem não encontrada, formato inválido, etc.)
     echo 'Erro ao processar a imagem: ' . $e->getMessage();
