1cb15bdeeaa3f164f37ed1dbaeedebc659ad9d45
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2024-10-16 23:36:20 -0300

    Update imagem.php

--- administracao/patrimonio/imagem.php
+++ administracao/patrimonio/imagem.php
@@ -1,6 +1,9 @@
 <?php
 require '../../vendor/autoload.php';
-use Intervention\Image\Facades\Image;
+use Intervention\Image\ImageManagerStatic as Image;
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 // Obter o nome da imagem do parâmetro GET
 $imagem = $_GET['imagem'];
@@ -13,13 +16,14 @@ try {
     $img = Image::make($caminho);
 
     // Redimensionar a imagem
-    $img->resize(null, 200);
+    $img->resize(null, 200, function ($constraint) {
+        $constraint->aspectRatio();
+    });
 
     // Definir o tipo de conteúdo e enviar a imagem
     header('Content-Type: image/jpeg'); // Ajustar o tipo conforme necessário
-    $img->response('jpeg');
+    return $img->response('jpeg');
 } catch (Exception $e) {
     // Tratar erros (imagem não encontrada, formato inválido, etc.)
     echo 'Erro ao processar a imagem: ' . $e->getMessage();
 }
-?>
