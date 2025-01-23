abf88749be39b458869f6382ce6437aeac3270ad
danielcadol <121764444+danielcardosoprojects@users.noreply.github.com>
2025-01-23 14:46:04 -0300

    tt

--- administracao/pagina_rh_home.php
+++ administracao/pagina_rh_home.php
@@ -235,7 +235,7 @@ include_once('../bd/nivel.php');
     $inicio = $pc - 1;
     $inicio = $inicio * $total_reg;
 
-
+    
 
     //busca u940659928_siupa.tb_funcionario
     $sqlbusca = "SELECT  DATE_FORMAT(f.data_nasc,'%d\/%m\/%Y') as data_nascbr, f.*, DATE_FORMAT(f.admissao,'%d\/%m\/%Y') as admissaoBR, f.id AS idfuncionario, c.descricao AS cargo, c.id, s.setor FROM u940659928_siupa.tb_funcionario AS f INNER JOIN u940659928_siupa.tb_cargo AS c ON f.fk_cargo = c.id INNER JOIN u940659928_siupa.tb_setor AS s ON f.fk_setor = s.id AND s.setor $where $fcsql $bsetorsql $orderby";
