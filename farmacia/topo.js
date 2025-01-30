d5daa07fbc9df6fe6ee491524b3753eab376cecb
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2025-01-29 22:15:56 -0300

    tt

--- farmacia/topo.js
+++ farmacia/topo.js
@@ -3,6 +3,7 @@ class MeuTopo extends HTMLElement {
         super();
         this.attachShadow({ mode: 'open' });
         this.validarUsuario();
+        this.contarItensVencidos();
     }
 
     async validarUsuario() {
@@ -41,7 +42,30 @@ class MeuTopo extends HTMLElement {
         }
     }
 
+    async contarItensVencidos() {
+        // Obtém a data atual no formato YYYY-MM-DD
+        const dataAtual = new Date().toISOString().split("T")[0];
+    
+        try {
+            // Faz a requisição para a API
+            const response = await fetch(`https://www.siupa.com.br/siiupa/api/api.php/records/tb_farmestoque?filter=data_validade,lt,${dataAtual}`);
+            const data = await response.json();
+    
+            // Retorna a quantidade de registros vencidos
+            const totalVencidos = data.records ? data.records.length : 0;
+            return totalVencidos;
+        } catch (error) {
+            console.error("Erro ao buscar itens vencidos:", error);
+            return 0; // Retorna 0 em caso de erro
+        }
+    }
+    
+    
+    
+
     render() {
+        
+   
         const style = document.createElement("style");
         style.textContent = `
             @import url("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css");
