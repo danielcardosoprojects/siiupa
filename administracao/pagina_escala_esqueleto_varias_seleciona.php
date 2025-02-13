bd59b44ceff39865d07fb8919b142058c465565d
danielcadol <121764444+danielcardosoprojects@users.noreply.github.com>
2025-02-13 12:19:35 -0300

    style seleciona varias

--- administracao/pagina_escala_esqueleto_varias_seleciona.php
+++ administracao/pagina_escala_esqueleto_varias_seleciona.php
@@ -33,13 +33,13 @@ function mesext($entrada)
     return $entrada;
 }
 ?>
-<html lang="pt">
-
+<!DOCTYPE html>
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Escalas Seleciona várias</title>
     <script src="/siiupa/js/jquery-3.6.0.js"></script>
     <!-- <link rel="stylesheet" href="/siiupa/bootstrap-5.0.0-beta3-dist/css/bootstrap.min.css"> -->
     <style>
