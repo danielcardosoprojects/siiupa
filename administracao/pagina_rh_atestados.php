9a2fbb2b13fff30321275b99389ad327ddf38851
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2025-02-13 22:54:40 -0300

    tt

--- administracao/pagina_rh_atestados.php
+++ administracao/pagina_rh_atestados.php
@@ -172,7 +172,11 @@ echo "</nav>";
 // AQUI TERMINA A PAGINAÇÃO HTML
 
 // Consulta SQL principal com LIMIT e OFFSET para paginação
-
+if(isset($_GET['nome'])){
+    $pesquisaNome = $_GET['nome'];
+} else {
+    $pesquisaNome = '';
+}
     $consulta_atestado = new BD;
     $sqlConsulta_Atestados = "
     SELECT 
@@ -194,6 +198,7 @@ echo "</nav>";
     INNER JOIN 
         u940659928_siupa.tb_afastamentos as afs 
         ON (A.fk_afastamentos = afs.id) 
+    WHERE f.nome LIKE %$pesquisaNome%
     ORDER BY 
         A.id DESC 
     LIMIT $registrosPorPagina OFFSET $offset
