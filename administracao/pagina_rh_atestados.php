a5fb4efb3437b933aac0264c0063bf38dbd676fb
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2025-02-13 22:09:24 -0300

    tt

--- administracao/pagina_rh_atestados.php
+++ administracao/pagina_rh_atestados.php
@@ -53,6 +53,8 @@ $paginaAtual = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
 // Calcula o offset para a consulta SQL
 $offset = ($paginaAtual - 1) * $registrosPorPagina;
 
+
+
 // Consulta SQL principal com LIMIT e OFFSET para paginação
 
     $consulta_atestado = new BD;
@@ -80,21 +82,9 @@ $offset = ($paginaAtual - 1) * $registrosPorPagina;
         A.id DESC 
     LIMIT $registrosPorPagina OFFSET $offset
 ";
-    $resultadoContagem = $consulta_atestado->consulta($sqlConsulta_Atestados);
-    $totalRegistros = $resultadoContagem[0]['total'];
+    $resultadoConsulta_Atestados = $consulta_atestado->consulta($sqlConsulta_Atestados);
 
-    // Calcula o número total de páginas
-    $totalPaginas = ceil($totalRegistros / $registrosPorPagina);
-   // Links de navegação entre as páginas
-echo "<div class='paginacao'>";
-for ($i = 1; $i <= $totalPaginas; $i++) {
-    if ($i == $paginaAtual) {
-        echo "<strong>$i</strong> ";
-    } else {
-        echo "<a href='?pagina=$i'>$i</a> ";
-    }
-}
-echo "</div>";
+   
 
     foreach ($resultadoConsulta_Atestados as $resultado_atestado) {
 
