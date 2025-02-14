6ea4accc1df30539539bf7ba91c8f9686249503b
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2025-02-13 22:50:02 -0300

    tt

--- administracao/pagina_rh_atestados.php
+++ administracao/pagina_rh_atestados.php
@@ -13,13 +13,13 @@ include_once('../bd/nivel.php');
 <hr>
 <br>
 
-<form id="buscaNomeAtestado" class="d-flex">
+<form id="searchForm" class="d-flex">
         <input type="text" id="searchInput" class="form-control me-2" placeholder="Digite um nome" required>
         <button type="submit" class="btn btn-primary">Pesquisar</button>
     </form>
 
     <script>
-         document.addEventListener("DOMContentLoaded", function() {
+        document.addEventListener("DOMContentLoaded", function() {
             document.getElementById("searchForm").addEventListener("submit", function(event) {
                 event.preventDefault(); // Evita recarregar a página
                 
