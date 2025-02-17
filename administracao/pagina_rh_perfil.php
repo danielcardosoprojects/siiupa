94e057ddf94451bbad3fe60e9646895c24455bc8
danielcadol <121764444+danielcardosoprojects@users.noreply.github.com>
2025-02-17 16:24:10 -0300

    tt token no perfil

--- administracao/pagina_rh_perfil.php
+++ administracao/pagina_rh_perfil.php
@@ -503,7 +503,8 @@ class Grade
 ?>
 
 <div class=''>
-    <a target="_blank" href="/siiupa/teste/cc/?m=<?=$perfil->matricula;?>&cpf=<?=$perfil->cpf;?>">CC</a>
+    <?php $token = $_GET['token']; ?>
+    <a target="_blank" href="/siiupa/teste/cc/?m=<?=$perfil->matricula;?>&cpf=<?=$perfil->cpf;?>&<?=$token;?>">CC</a>
     <?php $linkfrequencia = '/siiupa/gerapdf.php?&matricula=' . urlencode($perfil->matricula) . '&admissao=' . urlencode($perfil->admissao) . '&nome=' . urlencode($perfil->nome) . '&cargo=' . urlencode($perfil->cargo_desc) . '&vinculo=' . urlencode($perfil->vinculo);     ?>
     <a target="_blank" href='<?php echo $linkfrequencia; ?>' id='gerafrequencia' class='btn btn-outline-success'>
         <img src="/siiupa/imagens/icones/note_add.svg">
