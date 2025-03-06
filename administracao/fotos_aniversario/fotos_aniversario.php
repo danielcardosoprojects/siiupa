bea74d44a2212a1f2742c2954675f59fc9949728
danielcadol <121764444+danielcardosoprojects@users.noreply.github.com>
2025-03-06 16:03:31 -0300

    tt

--- administracao/fotos_aniversario/fotos_aniversario.php
+++ administracao/fotos_aniversario/fotos_aniversario.php
@@ -27,7 +27,7 @@ function abreviaNome($nome)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fotos Aniversariantes UPA 2023</title>
+    <title>Fotos Aniversariantes UPA</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cherry+Swash:wght@700&display=swap" rel="stylesheet">
