4157a0948f23e3dc76fe81ec040c322a278b72b3
danielcardosoprojects <danielcardoso.profissional@gmail.com>
2025-03-12 16:52:22 -0300

    refactor vacation month display in rh home page for improved readability

--- administracao/pagina_rh_home.php
+++ administracao/pagina_rh_home.php
@@ -435,9 +435,11 @@ include_once('../bd/nivel.php');
                     $ferias23 = new BD;
                     $sqlF23 = "SELECT * FROM u940659928_siupa.tb_ferias where fk_funcionario = '$dados->idfuncionario' and ref_ano = '2025';";
                     $rF23 = $ferias23->consulta($sqlF23);
+                    $meses = [];
                     foreach ($rF23 as $ferias23) {
-                        echo mes($ferias23->ref_mes) . '/';
+                        $meses[] = mes($ferias23->ref_mes);
                     }
+                    echo implode('/', $meses);
 
                     echo "</td>";
 
