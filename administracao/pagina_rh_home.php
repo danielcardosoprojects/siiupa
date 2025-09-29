20f949de1e194db3f70e2fd950fa21f22f48b0af
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2025-09-29 14:38:14 -0300

    Update pagina_rh_home.php

--- administracao/pagina_rh_home.php
+++ administracao/pagina_rh_home.php
@@ -250,7 +250,7 @@ include_once('../bd/nivel.php');
 
 
     //busca u940659928_siupa.tb_funcionario
-    $sqlbusca = "SELECT  DATE_FORMAT(f.data_nasc,'%d\/%m\/%Y') as data_nascbr, f.*, DATE_FORMAT(f.admissao,'%d\/%m\/%Y') as admissaoBR, f.id AS idfuncionario, c.descricao AS cargo, c.id, c.carga_horaria, s.setor FROM u940659928_siupa.tb_funcionario AS f INNER JOIN u940659928_siupa.tb_cargo AS c ON f.fk_cargo = c.id INNER JOIN u940659928_siupa.tb_setor AS s ON f.fk_setor = s.id $where $fcsql $bsetorsql $orderby";
+    $sqlbusca = "SELECT  DATE_FORMAT(f.data_nasc,'%d\/%m\/%Y') as data_nascbr, f.*, DATE_FORMAT(f.admissao,'%d\/%m\/%Y') as admissaoBR, f.id AS idfuncionario, c.descricao AS cargo, c.id, c.carga_horaria, c.cbo, s.setor FROM u940659928_siupa.tb_funcionario AS f INNER JOIN u940659928_siupa.tb_cargo AS c ON f.fk_cargo = c.id INNER JOIN u940659928_siupa.tb_setor AS s ON f.fk_setor = s.id $where $fcsql $bsetorsql $orderby";
 
 
 
