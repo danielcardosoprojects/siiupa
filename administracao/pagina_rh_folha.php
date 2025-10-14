117823d73d2f1b178f2d50cf6d6a271aa65c0aa5
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2025-10-14 10:48:57 -0300

    Update pagina_rh_folha.php

--- administracao/pagina_rh_folha.php
+++ administracao/pagina_rh_folha.php
@@ -359,13 +359,13 @@ if ($status_folha == "aberta") {
 </a> -->
 <hr id="inicio_folha">
 
+    <a class="btn btn-success" id="btAddServidor2" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+        <img src="/siiupa/imagens/icones/personadd.svg" width="24px" height="24px">
+
+    </a>
     <a class="btn btn-success" id="btAddServidor" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
         <img src="/siiupa/imagens/icones/personadd.svg">
         Adicionar servidor nesta folha
-    </a>
-    <a class="btn btn-success" id="btAddServidor2" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample2">
-        <img src="/siiupa/imagens/icones/personadd.svg" width="24px" height="24px">
-    
     </a>
 
 <?php
