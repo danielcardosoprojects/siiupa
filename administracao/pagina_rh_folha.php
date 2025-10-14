9734e1e2c69e5bdca06876ac9e202ddb3b73d390
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2025-10-14 11:11:31 -0300

    Update pagina_rh_folha.php

--- administracao/pagina_rh_folha.php
+++ administracao/pagina_rh_folha.php
@@ -868,7 +868,7 @@ function abrirAdicionarServidor(e) {
     window.addEventListener("scroll", () => {
       if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         btn.style.display = "flex";
-        btAddServidor.style.display = "flex";
+        btAddServidor.style.display = "fixed";
         btAddServidor.style.bottom = "20px";
         btAddServidor.style.right = "85px";
    
