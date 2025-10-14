823c072b1c2336a6d39e823cccdf6c19df09cc1a
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2025-10-14 10:09:56 -0300

    Update pagina_rh_folha.php

--- administracao/pagina_rh_folha.php
+++ administracao/pagina_rh_folha.php
@@ -363,7 +363,7 @@ if ($status_folha == "aberta") {
         Adicionar servidor nesta folha
     </a>
     <a class="btn btn-success" id="btAddServidor2" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
-        <img src="/siiupa/imagens/icones/personadd.svg">
+        <img src="/siiupa/imagens/icones/personadd.svg" width="24px" height="24px">
     
     </a>
 
