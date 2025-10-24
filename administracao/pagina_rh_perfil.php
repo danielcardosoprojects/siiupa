814318677b3e12fd8cfd272624d65d7263fb9611
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2025-10-24 10:49:10 -0300

    Update pagina_rh_perfil.php

--- administracao/pagina_rh_perfil.php
+++ administracao/pagina_rh_perfil.php
@@ -515,7 +515,7 @@ class Grade
 
 
     <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#modalCC">
-        Contracheque
+        Contracheques
     </button>
 
 
@@ -562,7 +562,7 @@ class Grade
         Editar dados
     </button>
 <br>
-<button onclick="baixarUltimoContracheque()" class="btn btn-outline-secondary" style="margin-top:5px">Visualizar último contracheque</button>
+<button onclick="baixarUltimoContracheque()" class="btn btn-outline-secondary" style="margin-top:5px">Último contracheque</button>
     <!-- Modal -->
     <div class="modal fade" id="modalEditarFuncionario" tabindex="-1" aria-labelledby="modalEditarFuncionarioLabel" aria-hidden="true">
         <div class="modal-dialog modal-xl modal-dialog-scrollable">
