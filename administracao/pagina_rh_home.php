d9e68e05262e602c22f6fec782157489101842c8
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2025-11-24 11:42:12 -0300

    Update pagina_rh_home.php

--- administracao/pagina_rh_home.php
+++ administracao/pagina_rh_home.php
@@ -477,7 +477,7 @@ include_once('../bd/nivel.php');
                     //COLS VACATIONS
                     echo "<td> $dataAdmissao </br>";
                     $ferias23 = new BD;
-                    $sqlF23 = "SELECT * FROM u940659928_siupa.tb_ferias where fk_funcionario = '$dados->idfuncionario' and ref_ano = '2026';";
+                    $sqlF23 = "SELECT * FROM u940659928_siupa.tb_ferias where fk_funcionario = '$dados->idfuncionario' and ref_ano = '2026' ORDER BY ref_mes ASC;";
                     $rF23 = $ferias23->consulta($sqlF23);
                     $meses = [];
                     foreach ($rF23 as $ferias23) {
