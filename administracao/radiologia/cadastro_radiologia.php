bc1c161d0c260af35dab301def4136f063e92869
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2026-07-21 16:05:59 -0300

    Update cadastro_radiologia.php

--- administracao/radiologia/cadastro_radiologia.php
+++ administracao/radiologia/cadastro_radiologia.php
@@ -11,14 +11,7 @@ $data_criacao = date('Y-m-d H:i:s');
     <title>Atualização Cadastral Radiologia UPA Castanhal</title>
     <style>
        
-        .container {
-            max-width: 600px;
-            margin: 0 auto;
-            background: #fff;
-            padding: 30px;
-            border-radius: 8px;
-            box-shadow: 0 2px 8px rgba(0,0,0,0.1);
-        }
+
         h1 {
             font-size: 22px;
             text-align: center;
